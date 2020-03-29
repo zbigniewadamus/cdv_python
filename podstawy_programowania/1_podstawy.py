5de7a59d50107fe3fa359ecf9080c4e227ff2937
Zbigniew Adamus <zs1adamus@gmail.com>
2020-03-29 14:15:30 +0200

    tak

--- podstawy_programowania/1_podstawy.py
+++ podstawy_programowania/1_podstawy.py
@@ -1,1 +1,64 @@
-print("cdv");
+#print("cdv");
+#potęgowanie
+#pow = 2 ** 10
+#print(pow)
+
+#text = "CDV"
+#print(text * 2)
+
+#pobieranie danych z klawiatury
+# + -> konkatenacja
+
+#name = input()
+
+#print("Twoje imie:" + name)
+
+#surname = input()
+
+#print("Twoje imie: " + name + ", nazwisko: " + surname)
+
+#length = len(surname)
+
+#print(type(surname))
+#print(type(name))
+#print(type(length))
+
+#konwersja na string
+#length = str(length)
+
+'''
+KOMENTARZ BLOKOWY
+'''
+#imie nazwisko wiek
+#imie nazwisko , wiek: 
+
+# camelcase -> firstLetterTest
+'''
+imie = input("Podaj imie")
+nazwisko = input("Podaj nazwisko")
+wiek = input("Podaj wiek")
+print(imie + " " + nazwisko + ", wiek: " + wiek)
+'''
+#konwersja
+'''
+x = "5"
+print(type(x))
+
+x=int(x)
+print(type(x))
+
+y = 4
+print(type(y))
+
+y = y / 2
+print(type(y))
+print(y)
+'''
+surname = "Kowalski"
+print()
+print(surname[0]) #K
+print(surname[0:3]) #Kow
+print(surname[-2]) #k
+print(surname[-2:]) #ki
+print(surname[:-2]) #Kowals
+print(surname[:-2:2]) #Kwl
